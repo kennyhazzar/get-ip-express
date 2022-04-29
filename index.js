c08d131eb0ebd2f8af1b1f450cc2a83f97fb9028
kennyhazzar <kolchanov@siberian.pro>
2022-04-29 22:53:48 +0700

    add dotenv, add cors rules

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 ﻿const express = require('express')
 const app = express()
+require('dotenv/config')
+const PORT = process.env.PORT
+
+app.use(express.json())
+app.use((_, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*'])
+    res.append('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS')
+    res.append('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next()
+})
 
 app.get('/ip', (req, res) => {
     res.send(req.ip)
 })
 
-app.listen(8000, () => console.log(`listening 8000`))
+app.listen(PORT, () => console.log(`listening: ${PORT}`))
