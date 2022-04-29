7dff684817496cb48bd559c0d1a10fdfb1f73e8b
kennyhazzar <kolchanov@siberian.pro>
2022-04-29 15:00:40 +0700

    init project

---
+++ index.js
@@ -0,0 +1,9 @@
+﻿const express = require('express')
+const app = express()
+const PORT = 8000
+
+app.get('/ip', (req, res) => {
+    res.send(req.ip)
+})
+
+app.listen(PORT, () => console.log(`listening ${PORT}`))
