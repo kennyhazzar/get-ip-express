3d9d9191e7cee2f66716d53da8907468841cdad2
kennyhazzar <kolchanov@siberian.pro>
2022-05-05 11:00:36 +0700

    refactor getting ip script on public

---
+++ public/js/getIp.js
@@ -0,0 +1,10 @@
+﻿fetch('/ip')
+    .then((response) => {
+        let $ip = document.querySelector("#ip__inner")
+        if (response.ok) {
+            response.json()
+                .then(({
+                    ip
+                }) => $ip.textContent = ip)
+        } else $ip.textContent = "Ошибка получения IP"
+    })
